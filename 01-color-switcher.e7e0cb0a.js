cff3878e897ee5c243daae8bcd407e7c15329e31
Nazar0105 <126713281+Nazar0105@users.noreply.github.com>
2023-07-22 12:59:20 +0000

    Deploying to gh-pages from @ Nazar0105/goit-js-hw-09@1a9959cd8e2950e5d3ff7d230ea8654d50268141 🚀

--- 01-color-switcher.e7e0cb0a.js
+++
@@ -1,2 +0,0 @@
-!function(){var e,t=document.querySelector("[data-start]"),n=document.querySelector("[data-stop]");function o(){var e=getRandomHexColor();document.body.style.backgroundColor=e}function a(){clearInterval(e),t.disabled=!1}t.addEventListener("click",(function(){a(),o(),e=setInterval(o,1e3),t.disabled=!0})),n.addEventListener("click",a)}();
-//# sourceMappingURL=01-color-switcher.e7e0cb0a.js.map
--- 01-color-switcher.f011c0ec.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]");let n;function o(){const e=getRandomHexColor();document.body.style.backgroundColor=e}function c(){clearInterval(n),e.disabled=!1}e.addEventListener("click",(function(){c(),o(),n=setInterval(o,1e3),e.disabled=!0})),t.addEventListener("click",c);
-//# sourceMappingURL=01-color-switcher.f011c0ec.js.map
